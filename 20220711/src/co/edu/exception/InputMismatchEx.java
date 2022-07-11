e37e039f746440489dad7599bbbd4adeafe17a42
admin <admin@YD-04>
2022-07-11 17:50:05 +0900

    20220711

---
+++ 20220711/src/co/edu/exception/InputMismatchEx.java
@@ -0,0 +1,25 @@
+package co.edu.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class InputMismatchEx {
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+		try {
+			System.out.println("숫자를 입력하세요.");
+			String num = scn.nextLine(); // 정수값 입력 => 문자 입력.
+			System.out.println("입력값 " + num);
+
+		} catch (InputMismatchException e) {
+			System.out.println("잘못된 값 입력됨.");
+
+		}
+
+		System.out.println("프로그램을 종료.");
+
+	}
+
+}
---
+++ 20220711/src/co/edu/exception/ThrowException.java
@@ -0,0 +1,27 @@
+package co.edu.exception;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ThrowException {
+	public static void main(String[] args) {
+		
+		try {
+			method1();
+		} catch (ClassNotFoundException| SQLException e) {
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	public static void method1 () throws ClassNotFoundException, SQLException {
+		
+		Class.forName("java.lang.String");
+		DriverManager.getConnection("", "","");
+		
+				
+	}
+
+}
