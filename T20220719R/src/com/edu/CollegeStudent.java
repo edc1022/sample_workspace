0e5851996c6e06431314378df207e4eadc9275e9
admin <admin@YD-04>
2022-07-19 19:00:22 +0900

    재시 과제

---
+++ T20220719R/src/com/edu/CollegeStudent.java
@@ -0,0 +1,22 @@
+package com.edu;
+
+public class CollegeStudent extends Student {
+//	대학생정보 : 담당교수, 전공과목 정보 추가.
+
+	private 
+	private
+	
+	
+	
+	// 생성자.
+public CollegeStudent () {
+		
+	
+	}
+	
+	
+	
+	
+	// toString()
+
+}
---
+++ T20220719R/src/com/edu/HighStudent.java
@@ -0,0 +1,29 @@
+package com.edu;
+
+public class HighStudent extends Student {
+//	고등학생정보 : 담임교사, 학년 정보 추가.
+    private 
+    private
+	
+	
+	
+	
+	
+	// 생성자.
+
+	public HighStudent () {
+    	
+   
+    
+    
+    }
+	
+	
+	
+	
+	// toString()
+
+	
+	
+	
+}
