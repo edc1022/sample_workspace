bd63edd5d4eb23c00b4d4513db1aa66af6c73d6f
admin <admin@YD-04>
2022-07-20 17:53:20 +0900

    20220720

--- T20220719R/src/com/edu/CollegeStudent.java
+++ T20220719R/src/com/edu/CollegeStudent.java
@@ -3,20 +3,48 @@ package com.edu;
 public class CollegeStudent extends Student {
 //	대학생정보 : 담당교수, 전공과목 정보 추가.
 
-	private 
-	private
+	private String pro;
+	private String major;
 	
 	
 	
 	// 생성자.
-public CollegeStudent () {
-		
+
+
+public CollegeStudent(String pro,String major) {
+	super();
+	this.pro = pro;
+	this.major = major;
 	
-	}
+}
+	
+public CollegeStudent() {
+	
+}
+
+public String getPro() {
+	return pro;
+}
+
+public void setPro(String pro) {
+	this.pro = pro;
+}
+
+public String getMajor() {
+	return major;
+}
+
+public void setMajor(String major) {
+	this.major = major;
+}
+
+// toString()
+@Override
+public String toString() {
+	return "CollegeStudent [studNo=" + getStudNo() + ", studName=\" + getStudName() + ", score=\" + getScore() + " pro=" + pro + ", major=" + major + "]";
+}
 	
 	
-	
-	
-	// toString()
+
 
 }
